13c44e1c1a43d1abafb7c893cb23d4353e76fe88
James McAllister <james@mcallister.xyz>
2017-04-12 10:36:20 +0100

    fixed export problem

--- rollup.config.js
+++ rollup.config.js
@@ -2,17 +2,17 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import filesize from 'rollup-plugin-filesize';
 import uglify from 'rollup-plugin-uglify';
-import { minify } from 'uglify-js-harmony';
+import {minify} from 'uglify-js-harmony';
 
 export default {
   entry: 'main.js',
   dest: 'bundle.js',
   moduleName: 'styledComponentsTachyons',
-  format: 'iife',
+  format: 'cjs',
   plugins: [
-    resolve({ jsnext: true, main: true }),
     commonjs(),
+    resolve({jsnext: true, main: true}),
+    uglify({}, minify),
     filesize(),
-    uglify({}, minify)
-  ]
+  ],
 };
